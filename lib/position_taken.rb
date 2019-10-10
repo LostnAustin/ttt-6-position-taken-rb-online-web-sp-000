edd1b0699be4363b7fa5e6f5cd3621f94a77c965
Chad Alan Lengefeld <chadlengefeld@gmail.com>
2019-10-10 19:20:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if (index == nil) || (index == " ") || (index == "")
-     puts false
+     return false
   elsif (index == "X") || (index == "O")
     return true
   end
